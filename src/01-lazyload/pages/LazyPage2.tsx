590da6dc7a7245e51648ddea602aa6ad2a586494
Diegoabustos <you@example.com>
2022-09-10 14:13:27 -0500

    LazyLoad Basic ready

--- src/01-lazyload/pages/LazyPage2.tsx
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -4,3 +4,5 @@ export const LazyPage2 = () => {
     <div>LazyPage1</div>
   )
 }
+
+export default LazyPage2;
--- src/01-lazyload/pages/LazyPage3.tsx
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -3,4 +3,6 @@ export const LazyPage3 = () => {
   return (
     <div>LazyPage1</div>
   )
-}
+};
+
+export default LazyPage3;
